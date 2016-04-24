11c7a0b1b7d3bb3abaa3a37bf418e690c59e4a20
ysnrkdm <euphonictech@gmail.com>
2016-04-24 15:17:57 +0900

    Fix ProgressPositionMovesLearn. Add debugger in paramgen

--- paramgen.py
+++ paramgen.py
@@ -1,4 +1,6 @@
-import simple_learn
+import config
+import sys
+import replearn
 
 
 def conv_num(num):
@@ -18,16 +20,45 @@ def write_data(file_full_path, parameters):
         fout.write(bary)
 
 
-def fetch_and_paramgen():
+def fetch_and_paramgen(conf):
     # load parameter
-    a = simple_learn.SimpleLearn()
-    params = a.read_parameters()
-    write_data('', params)
+    learn = replearn.get_learn(conf)
+    print 'type is %s' % type(learn)
+    learn.configure(conf)
+    params = learn.read_parameters()
+    print 'params: %s' % str(params)
+    write_data(conf['paramgen_output_path'], params)
 
 
-def main():
-    fetch_and_paramgen()
+def fetch_and_show(conf):
+    file_full_path = conf['paramgen_output_path']
+    with open(file_full_path, "rb") as fin:
+        data = fin.read()
+        N = len(data)
+        line = ''
+        print 'data length: %d' % N
+        for i in range(N):
+            elem = str(ord(data[i]))
+            if i == 0:
+                print elem
+            elif (i) % ((N-2)/4) == 0 and i != 1:
+                line += elem + ', '
+                print line
+                line = ''
+            else:
+                line += elem + ', '
+        print line
+
+
+def main(comm, file_full_path):
+    conf = config.config_by_filename(file_full_path)
+    if comm == 'read':
+        fetch_and_show(conf)
+    elif comm == 'write':
+        fetch_and_paramgen(conf)
 
 
 if __name__ == '__main__':
-    main()
+    command = sys.argv[1]
+    filename = sys.argv[2]
+    main(command, filename)
