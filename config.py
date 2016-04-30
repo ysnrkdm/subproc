fdf6b4eab1599baab865cd63c0ce034dee11dc77
ysnrkdm <euphonictech@gmail.com>
2016-04-30 16:46:02 +0900

    Read config structure as-is. No handcraft dict mapping

--- config.py
+++ config.py
@@ -4,30 +4,15 @@ from ConfigParser import SafeConfigParser
 def config_by_filename(filename):
     parser = SafeConfigParser()
     parser.read(filename)
-    ret = {
-        # [redis]
-        'redis_hostname': parser.get('redis', 'hostname'),
-        'redis_port': parser.get('redis', 'port'),
-        'redis_password': parser.get('redis', 'password'),
-        'redis_db_book': parser.getint('redis', 'book_db'),
-        'redis_db_param': parser.getint('redis', 'param_db'),
-        'redis_book_dbkeyprefix': parser.get('redis', 'book_dbkeyprefix'),
-        # [proc]
-        'proc_a_path': parser.get('proc', 'a_path'),
-        'proc_b_path': parser.get('proc', 'b_path'),
-        #[paramgen]
-        'paramgen_output_path': parser.get('paramgen', 'output_path'),
-        #[learn]
-        'learn_from': parser.get('learn', 'from'),
-        'learn_class': parser.get('learn', 'class'),
-        'learn_epic_batch_size': parser.getint('learn', 'epic_batch_size'),
-        #[game_reader]
-        'game_reader_from': parser.get('game_reader', 'from'),
-        'game_reader_class': parser.get('game_reader', 'class'),
-        #[game_recorder]
-        'game_recorder_from': parser.get('game_recorder', 'from'),
-        'game_recorder_class': parser.get('game_recorder', 'class'),
-    }
+    ret = dict()
+    for section in parser.sections():
+        for key, value in parser.items(section):
+            rvalue = value
+            try:
+                rvalue = int(value)
+            except:
+                pass
+            ret[section + '_' + key] = rvalue
     return ret
 
 
